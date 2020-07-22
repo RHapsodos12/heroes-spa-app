2a933e29f23cbb8384b4b608330df302d877e572
David Santacruz <davidsantacruz09@gmail.com>
2020-07-21 22:33:03 -0500

    ''

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes: Heroe[] = [];
+
+  // tslint:disable-next-line:variable-name
+  constructor(private _heroesService: HeroesService) {
+    console.log('Constructor');
+  }
+
+  ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+    // console.log(this.heroes);
+  }
+
+}
