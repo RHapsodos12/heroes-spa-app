1cefad67183f1a4498797a6d972861b4bb555a00
David Santacruz <davidsantacruz09@gmail.com>
2021-09-24 22:08:09 -0500

    + heroe component

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-heroes',
@@ -11,13 +13,31 @@ export class HeroesComponent implements OnInit {
   heroes: Heroe[] = [];
 
   // tslint:disable-next-line:variable-name
-  constructor(private _heroesService: HeroesService) {
-    console.log('Constructor');
+  constructor(private _heroesService: HeroesService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+    // console.log('Constructor');
   }
 
   ngOnInit(): void {
-    this.heroes = this._heroesService.getHeroes();
+
+    this.activatedRoute.params.subscribe( params => {
+
+      if (params.name) {
+        // console.log(params.name);
+        // tslint:disable-next-line:no-string-literal
+        this.heroes = this._heroesService.buscarHeroes(params['name']);
+        // console.log(this._heroesService.buscarHeroes(params['name']));
+      } else {
+        this.heroes = this._heroesService.getHeroes();
+      }
+    });
     // console.log(this.heroes);
   }
 
+  // tslint:disable-next-line: typedef
+  verHeroe(idx: number) {
+    // console.log(idx);
+    this.router.navigate( ['/heroe', idx] );
+  }
 }
